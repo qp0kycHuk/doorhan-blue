358847f6013bd69ec538b9c4b05958e85401f581
Александр Актищев <aktishch@gmail.com>
2022-10-06 16:46:27 +0300

    accordion

---
+++ js/accordion.js
@@ -0,0 +1,58 @@
+const accordions = document.querySelectorAll('*[data-accordion]')
+
+accordions.forEach((item) => {
+
+    const accordion = item
+    const accordionItem = item.querySelectorAll('*[data-child]')
+    const toggle = accordion.querySelector('*[data-toggle]')
+    const content = accordion.querySelector('*[data-content]')
+
+    if (accordion.dataset.accordion == 'hidden') content.style.height = '0'
+
+    if (accordion.dataset.accordion == 'active') content.style.height = `${content.scrollHeight}px`
+
+    toggle.addEventListener('click', () => {
+
+        if (accordion.dataset.accordion == 'hidden') {
+
+            accordion.dataset.accordion = 'active'
+            content.style.height = `${content.scrollHeight}px`
+
+        } else {
+
+            accordion.dataset.accordion = 'hidden'
+            content.style.height = '0'
+
+        }
+
+    })
+
+    accordionItem.forEach(el => {
+
+        const childToggle = el.querySelector('*[data-child-toggle]')
+        const childContent = el.querySelector('*[data-child-content]')
+
+        if (el.dataset.child == 'hidden') childContent.style.height = '0'
+
+        if (el.dataset.child == 'active') childContent.style.height = `${childContent.scrollHeight}px`
+
+        childToggle.addEventListener('click', () => {
+
+            if (el.dataset.child == 'hidden') {
+
+                el.dataset.child = 'active'
+                childContent.style.height = `${childContent.scrollHeight}px`
+                content.style.height = `${content.scrollHeight + childContent.scrollHeight}px`
+            } else {
+
+                el.dataset.child = 'hidden'
+                childContent.style.height = '0'
+                content.style.height = `${content.scrollHeight - childContent.scrollHeight}px`
+
+            }
+
+        })
+
+    })
+
+})
