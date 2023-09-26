e664221a240c7c36932abdd5c7c1ad1da176d82f
Александр Актищев <aktishch@gmail.com>
2023-09-26 10:25:10 +0300

    menu scroll

--- js/menuScroll.js
+++ js/menuScroll.js
@@ -1,23 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('.nav');
-    const hero = document.querySelector('.hero');
-    window.addEventListener('scroll', () => {
-        let scrollTop = window.scrollY;
-        let heroTop = hero.offsetHeight / 20;
-        if(scrollTop >= heroTop){
-            nav.classList.add('fixed');
-            nav.style.top = `-${nav.offsetHeight}px`;
-            hero.style.marginTop = `${nav.offsetHeight}px`;
-        }else{
-            nav.classList.remove('fixed');
-            nav.style.top = "auto";
-            hero.style.marginTop = `0px`;
+    const init = () => {
+        const header = document.querySelector('.nav')
+      
+        if (!header) return
+      
+        let prevOffsetTop = document.documentElement.scrollTop
+      
+        const scrollHeader = () => {
+          const currentOffsetTop = document.documentElement.scrollTop
+          const headerHeight = header.offsetHeight
+      
+          prevOffsetTop > currentOffsetTop
+            ? header.style.setProperty('--top', '0')
+            : header.style.setProperty('--top', `-${headerHeight}px`)
+      
+          prevOffsetTop = currentOffsetTop
         }
-        let heroCenter = hero.offsetHeight;
-        if(scrollTop >= heroCenter){
-            nav.style.top = "0px"
-        }else{
-            nav.style.top = `-${nav.offsetHeight}px`;
-        }
-    });
+      
+        document.addEventListener('scroll', scrollHeader)
+    }
+
+    init()
 });
